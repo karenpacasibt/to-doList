dbd5a7f77ada0281db51afcc661167e1155348d8
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-15 10:07:33 -0400

    4: update models and migrations

--- database/migrations/2025_04_15_133037_create_tasktags_table.php
+++ database/migrations/2025_04_15_133037_create_tasktags_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tasktag', function (Blueprint $table) {
+        Schema::create('tasktags', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_task');
             $table->foreignId('id_tag');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tasktag');
+        Schema::dropIfExists('tasktags');
     }
 };
