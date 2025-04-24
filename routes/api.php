e73824fcf8b63a91d81e6e9a85d835dc70a36809
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-24 15:06:24 -0400

    Create of the tags API

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,9 @@ Route::post("/category", [CategoryController::class,"store"]);
 Route::put("/category/{id}", [CategoryController::class,"update"]);
 Route::delete("/category/{id}", [CategoryController::class,"destroy"]);
 
+//RUTAS DE TAGS
+Route::get("/tag", [TagController::class,"index"]);
+Route::get("/tag/{id}", [TagController::class,"show"]);
+Route::post("/tag", [TagController::class,"store"]);
+Route::put("/tag/{id}", [TagController::class,"update"]);
+Route ::delete("/tag/{id}", [TagController::class,"destroy"]);
