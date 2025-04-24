5a188815a7a5d29c0bd51d7e9e72aaae2f7781c9
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-24 10:36:40 -0400

    Revert "15: Create of the Category API"
    This reverts commit 6712fcded3e2ea16fa07c7de6d5e3fd6f1c4839e.

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\TaskController;
-use App\Http\Controllers\Api\TagController;
-use App\Http\Controllers\Api\CategoryController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,23 +14,6 @@ use App\Http\Controllers\Api\CategoryController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-//RUTAS DE TASK 
-Route::get("/task", [TaskController::class,"index"]);
-Route::get("/task/{id}", [TaskController::class,"show"]);
-Route::post("/task", [TaskController::class,"store"]);
-Route::put("/task/{id}", [TaskController::class,"update"]);
-
-//RUTAS DE TAGS
-Route::get("/tag", [TagController::class,"index"]);
-Route::get("/tag/{id}", [TagController::class,"show"]);
-Route::post("/tag", [TagController::class,"store"]);
-Route::put("/tag/{id}", [TagController::class,"update"]);
-
-//RUTAS DE CATEGORIES
-Route::get("/category", [CategoryController::class,"index"]);
-Route::get("/category/{id}", [CategoryController::class,"show"]);
-Route::post("/category", [CategoryController::class,"store"]);
-Route::put("/category/{id}", [CategoryController::class,"update"]);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
