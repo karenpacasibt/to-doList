ca9341bce183c5b589c1c4e041004742099c5955
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-25 10:34:32 -0400

    Update task api

--- routes/api.php
+++ routes/api.php
@@ -6,16 +6,6 @@ use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//RUTAS DE CATEGORIES
-Route::get("/category", [CategoryController::class,"index"]);
-Route::get("/category/{id}", [CategoryController::class,"show"]);
-Route::post("/category", [CategoryController::class,"store"]);
-Route::put("/category/{id}", [CategoryController::class,"update"]);
-Route::delete("/category/{id}", [CategoryController::class,"destroy"]);
+Route::apiResource("category", CategoryController::class);
 
-//RUTAS DE TASK 
-Route::get("/task", [TaskController::class,"index"]);
-Route::get("/task/{id}", [TaskController::class,"show"]);
-Route::post("/task", [TaskController::class,"store"]);
-Route::put("/task/{id}", [TaskController::class,"update"]);
-Route::delete( "/task/{id}",[TaskController::class,"destroy"]);
+Route::apiResource("task", TaskController::class);
