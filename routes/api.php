6699203b30b6b9a867a78ee54e73e03c93965514
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-25 10:44:49 -0400

    update tag api

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,6 @@ use App\Http\Controllers\Api\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//RUTAS DE CATEGORIES
-Route::get("/category", [CategoryController::class,"index"]);
-Route::get("/category/{id}", [CategoryController::class,"show"]);
-Route::post("/category", [CategoryController::class,"store"]);
-Route::put("/category/{id}", [CategoryController::class,"update"]);
-Route::delete("/category/{id}", [CategoryController::class,"destroy"]);
+Route::apiResource("category", CategoryController::class);
 
-//RUTAS DE TAGS
-Route::get("/tag", [TagController::class,"index"]);
-Route::get("/tag/{id}", [TagController::class,"show"]);
-Route::post("/tag", [TagController::class,"store"]);
-Route::put("/tag/{id}", [TagController::class,"update"]);
-Route ::delete("/tag/{id}", [TagController::class,"destroy"]);
+Route::apiResource("tag", TagController::class);
