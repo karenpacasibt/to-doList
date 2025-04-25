5da18d2cafe83e53a24206844931e01f6135aa5f
Romina Pacasi <romina.pacasi.btsoft@gmail.com>
2025-04-25 10:46:54 -0400

    update API tag

--- app/Http/Controllers/Api/TagController.php
+++ app/Http/Controllers/Api/TagController.php
@@ -17,6 +17,7 @@ class TagController extends Controller
         ];
         return response()->json($data, 200);
     }
+
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -27,11 +28,13 @@ class TagController extends Controller
         $tag->save();
         return response()->json(['data' => $tag], 200);
     }
+
     public function show($id)
     {
         $tag = Tag::findOrFail($id);
         return response()->json(['data' => $tag], 200);
     }
+
     public function update(Request $request, $id)
     {
         $tag = Tag::findOrFail($id);
@@ -40,11 +43,13 @@ class TagController extends Controller
         ]);
         $tag->name = $validated['name'];
         $tag->save();
-        return response()->json(['data'=> $tag], 201);
+        return response()->json(['data' => $tag], 201);
     }
-    public function destroy($id){
+    
+    public function destroy($id)
+    {
         $tag = Tag::findOrFail($id);
         $tag->delete();
-        return response()->json(['data'=> $tag], 201);
+        return response()->json(['data' => $tag], 201);
     }
 }
